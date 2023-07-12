3eb4ad51584da53205698532ac3b4b83bc737376
wizbao <1414626899@qq.com>
2023-07-12 23:15:29 +0800

    improve 配置更多参数

--- java-pro/pulsargo/src/main/java/com/abot/pulsargo/service/SimpleConsumer.java
+++ java-pro/pulsargo/src/main/java/com/abot/pulsargo/service/SimpleConsumer.java
@@ -15,13 +15,40 @@ public class SimpleConsumer {
     private PulsarClient client;
 
     public void consumeMsg() throws PulsarClientException {
+        // MessageListener避免线程阻塞，符合solid原则
+        /*MessageListener myMessageListener = (consumer, msg) -> {
+            try {
+                System.out.println("Message received: " + new String(msg.getData()));
+                consumer.acknowledge(msg);
+            } catch (Exception e) {
+                consumer.negativeAcknowledge(msg);
+            }
+        };*/
+
+        //======================================================
         // 使用客户端创建消费者
         Consumer consumer = client.newConsumer()
                 .topic("my-topic")
                 .subscriptionName("my-subscription")
+                .subscriptionType(SubscriptionType.Shared)
+//                .readCompacted(true)  // 读取压缩主题 注意：Read compacted can only be used with exclusive or failover persistent subscriptions
                 .subscribe();
-        Message msg = consumer.receive();
-        System.out.println("==========================="+new String(msg.getData()));
+        while (true) {
+            // Wait for a message
+            Message msg = consumer.receive();
+
+            try {
+                // Do something with the message
+                System.out.println("Message received: " + new String(msg.getData()));
+
+                // Acknowledge the message
+                consumer.acknowledge(msg);
+            } catch (Exception e) {
+                // Message failed to process, redeliver later
+                consumer.negativeAcknowledge(msg);
+            }
+        }
+
     }
 }
 
--- java-pro/pulsargo/src/main/java/com/abot/pulsargo/service/SimpleProducer.java
+++ java-pro/pulsargo/src/main/java/com/abot/pulsargo/service/SimpleProducer.java
@@ -7,6 +7,7 @@ import org.apache.pulsar.client.api.Schema;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
 
 
 /**
@@ -17,11 +18,17 @@ public class SimpleProducer {
     @Resource
     private PulsarClient client;
 
-    public void sendMsg(String msg) throws PulsarClientException {
+    public void sendMsg() throws PulsarClientException {
         // 使用客户端创建生产者
-        Producer<String> stringProducer = client.newProducer(Schema.STRING)
+        Producer<String> producer = client.newProducer(Schema.STRING)
+                .producerName("")
+                .sendTimeout(0,TimeUnit.SECONDS)
                 .topic("my-topic")
                 .create();
-        stringProducer.send(msg);
+        producer.newMessage()
+                .key("my-key")  // key和value是消息密钥，用于主题压缩
+                .value("helloworld")
+                .deliverAfter(60, TimeUnit.SECONDS)  // 延迟发送 仅共享和密钥共享订阅支持延迟消息传递。在其他订阅中，延迟的消息会立即发送。默认的订阅类型是独占
+                .send();
     }
 }
