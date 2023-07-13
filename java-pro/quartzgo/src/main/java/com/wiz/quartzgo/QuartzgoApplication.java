17e893e055671d2073689dd9b68ef2475c24fd36
zhangweibao <xiangyangcao@7lk.com>
2023-07-13 18:35:11 +0800

    A 新增定时任务模块

---
+++ java-pro/quartzgo/src/main/java/com/wiz/quartzgo/QuartzgoApplication.java
@@ -0,0 +1,13 @@
+package com.wiz.quartzgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartzgoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuartzgoApplication.class, args);
+    }
+
+}
---
+++ java-pro/quartzgo/src/test/java/com/wiz/quartzgo/QuartzgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wiz.quartzgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuartzgoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
